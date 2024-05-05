d1d47db20cf08e14738d3f600a9d46164abd566e
lutfur rahman siddiquee <121648644+lutfurrahman20@users.noreply.github.com>
2024-05-05 18:15:10 +0600

    simple navigation var added

--- first_app/views.py
+++ first_app/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
 
+from django.http import HttpResponse
 # Create your views here.
+
+def contact(request):
+    return HttpResponse('''
+                        <h1>This is my contact page</h1>
+                        <a href = '/first_app/about/'>about</a>
+                        <a href = '/second_app/courses/'>courses</a>
+                        <a href = '/second_app/feedback/'>feedback</a>
+                        ''')
+def about(request):
+    return HttpResponse('''
+                        <h1>This is my about page</h1>
+                        <a href = '/first_app/contact/'>contact</a>
+                        <a href = '/second_app/courses/'>courses</a>
+                        <a href = '/second_app/feedback/'>feedback</a>
+                        ''')
--- second_app/views.py
+++ second_app/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+
+def courses(request):
+    return HttpResponse('''
+                        <h1>This is my courses page</h1>
+                        <a href = '/second_app/feedback/'>feedback<br></a>
+                        <a href = '/first_app/about/'>about<br></a>
+                        <a href = '/first_app/contact/'>contact</a>
+                        
+                        ''')
+def feedback(request):
+    return HttpResponse('''
+                        <h1>This is my feedback page</h1>
+                        <a href = '/second_app/courses/'>courses</a>
+                        <a href = '/first_app/about/'>about</a>
+                        <a href = '/first_app/contact/'>contact</a>
+                        
+                        ''')
